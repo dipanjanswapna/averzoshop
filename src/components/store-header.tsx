d952b1940b49f5100353d1c6b903872412f5bd85
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-01-15 07:34:28 +0000

    Try fixing this error: `Runtime ReferenceError: Input is not defined. Er

--- src/components/store-header.tsx
+++ src/components/store-header.tsx
@@ -27,6 +27,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { Input } from '@/components/ui/input';
 
 export function StoreHeader() {
   return (
