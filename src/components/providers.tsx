db25fdc0f6900b10b40202d7fbcd3f1a2de5f2a2
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-01-17 00:13:53 +0000

    Try fixing this error: `Runtime Error: `Tooltip` must be used within `To

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,17 +1,19 @@
-
 'use client';
 
 import * as React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>
-      <FirebaseClientProvider>{children}</FirebaseClientProvider>
+      <FirebaseClientProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </FirebaseClientProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
