fb36680472ae13c240d82229a3b058cc9db7b117
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-01-18 21:55:40 +0000

    Try fixing this error: `Runtime ReferenceError: useEffect is not defined

--- src/app/(protected)/customer/my-wishlist/page.tsx
+++ src/app/(protected)/customer/my-wishlist/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import { useAuth } from '@/hooks/use-auth';
 import { useFirestoreQuery } from '@/hooks/useFirestoreQuery';
 import { Wishlist, WishlistItem } from '@/types/wishlist';
