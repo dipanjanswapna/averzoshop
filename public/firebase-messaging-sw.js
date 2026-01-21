ee6b412a1901c825b5eeb051f811d341b6e32202
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-01-21 18:28:11 +0000

    ব্রাউজারের মাধ্যমে আপনি যেকোনো ওয়েবসাইটকে একটি অ্যাপের মতো আপনার পিসি বা

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,31 +1,35 @@
-// This file must be in the public directory
+
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-messaging-compat.js');
 
-const urlParams = new URLSearchParams(location.search);
+// Initialize the Firebase app in the service worker by passing in
+// the messagingSenderId.
+const urlParams = new URLSearchParams(self.location.search);
 const firebaseConfigParam = urlParams.get('firebaseConfig');
 
 if (firebaseConfigParam) {
-    try {
-        const firebaseConfig = JSON.parse(firebaseConfigParam);
-        if (firebaseConfig) {
-            firebase.initializeApp(firebaseConfig);
+    const firebaseConfig = JSON.parse(firebaseConfigParam);
 
-            const messaging = firebase.messaging();
+    if (firebaseConfig) {
+        firebase.initializeApp(firebaseConfig);
 
-            messaging.onBackgroundMessage(function(payload) {
-                console.log('Received background message ', payload);
+        // Retrieve an instance of Firebase Messaging so that it can handle background
+        // messages.
+        const messaging = firebase.messaging();
 
-                const notificationTitle = payload.notification.title;
-                const notificationOptions = {
-                    body: payload.notification.body,
-                    icon: '/icons/icon-192x192.png'
-                };
+        messaging.onBackgroundMessage(function(payload) {
+          console.log('[firebase-messaging-sw.js] Received background message ', payload);
+          // Customize notification here
+          const notificationTitle = payload.notification.title;
+          const notificationOptions = {
+            body: payload.notification.body,
+            icon: payload.notification.image
+          };
 
-                self.registration.showNotification(notificationTitle, notificationOptions);
-            });
-        }
-    } catch (e) {
-        console.error('Error parsing Firebase config in service worker:', e);
+          self.registration.showNotification(notificationTitle, notificationOptions);
+        });
     }
 }
