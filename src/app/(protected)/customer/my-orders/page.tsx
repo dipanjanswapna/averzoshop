a416d181223e113f301560c627b311ddfcdde067
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-01-21 08:38:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(protected)/customer/my-orders/page.tsx
+++ src/app/(protected)/customer/my-orders/page.tsx
@@ -113,35 +113,36 @@ export default function MyOrdersPage() {
     });
   };
 
-  const renderSkeleton = () => (
+  const renderDesktopSkeleton = () => (
     <>
-        <tbody className="hidden md:table-row-group">
-            {[...Array(3)].map((_, i) => (
-                <TableRow key={i}>
-                    <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-                    <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-                    <TableCell><Skeleton className="h-6 w-20 rounded-full" /></TableCell>
-                    <TableCell className="text-right"><Skeleton className="h-5 w-16 ml-auto" /></TableCell>
-                    <TableCell className="text-right"><Skeleton className="h-9 w-28 ml-auto" /></TableCell>
-                </TableRow>
-            ))}
-        </tbody>
-         <div className="grid md:hidden gap-4">
-            {[...Array(3)].map((_, i) => (
-              <Card key={i}>
-                <CardHeader>
-                    <Skeleton className="h-5 w-3/4" />
-                    <Skeleton className="h-4 w-1/2" />
-                </CardHeader>
-                <CardContent>
-                    <Skeleton className="h-6 w-24" />
-                </CardContent>
-                <CardFooter>
-                    <Skeleton className="h-9 w-full" />
-                </CardFooter>
-              </Card>
-            ))}
-        </div>
+        {[...Array(3)].map((_, i) => (
+            <TableRow key={i}>
+                <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+                <TableCell><Skeleton className="h-5 w-24" /></TableCell>
+                <TableCell><Skeleton className="h-6 w-20 rounded-full" /></TableCell>
+                <TableCell className="text-right"><Skeleton className="h-5 w-16 ml-auto" /></TableCell>
+                <TableCell className="text-right"><Skeleton className="h-9 w-28 ml-auto" /></TableCell>
+            </TableRow>
+        ))}
+    </>
+  );
+
+  const renderMobileSkeleton = () => (
+     <>
+        {[...Array(3)].map((_, i) => (
+          <Card key={i}>
+            <CardHeader>
+                <Skeleton className="h-5 w-3/4" />
+                <Skeleton className="h-4 w-1/2" />
+            </CardHeader>
+            <CardContent>
+                <Skeleton className="h-6 w-24" />
+            </CardContent>
+            <CardFooter>
+                <Skeleton className="h-9 w-full" />
+            </CardFooter>
+          </Card>
+        ))}
     </>
   );
 
@@ -195,7 +196,7 @@ export default function MyOrdersPage() {
                 </TableRow>
                 </TableHeader>
                 <TableBody>
-                {isLoading ? renderSkeleton() : sortedUserOrders && sortedUserOrders.length > 0 ? (
+                {isLoading ? renderDesktopSkeleton() : sortedUserOrders && sortedUserOrders.length > 0 ? (
                     sortedUserOrders.map(order => (
                     <TableRow key={order.id}>
                         <TableCell className="font-medium">
@@ -235,7 +236,7 @@ export default function MyOrdersPage() {
 
           {/* Mobile Cards */}
           <div className="grid md:hidden gap-4">
-              {isLoading ? renderSkeleton() : sortedUserOrders && sortedUserOrders.length > 0 ? (
+              {isLoading ? renderMobileSkeleton() : sortedUserOrders && sortedUserOrders.length > 0 ? (
                 sortedUserOrders.map(order => (
                     <Card key={order.id} className="flex flex-col">
                         <CardHeader>
