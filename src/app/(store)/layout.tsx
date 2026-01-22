5b8cf96551db6230711663148b3b75174022ecd2
Dipanjan swapna Prangon <dipanjanswapnaprangon@gmail.com>
2026-01-22 05:58:17 +0000

    We Accept: er sobiti aro boro koro

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -102,8 +102,8 @@ export default function StoreLayout({
                       <Image
                           src="https://securepay.sslcommerz.com/public/image/SSLCommerz-Pay-With-logo-All-Size-03.png"
                           alt="SSLCommerz Payment Gateways"
-                          width={400}
-                          height={58}
+                          width={500}
+                          height={72}
                       />
                   </a>
               </div>
